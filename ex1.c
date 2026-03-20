d1c6558a37038d2518f936b9f90da33af81bbc1f
g1n9err <chy.korn@proton.me>
2026-03-21 02:15:34 +0400

    Homework on the equations of the numeral system and the C language

---
+++ ex1.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+int main()
+{
+	int x;
+	int y;
+	
+	printf("Enter x: ");
+	scanf("%d", &x);
+	printf("Enter y: ");
+	scanf("%d", &y);
+	printf("x + y = %d\n", x + y);
+
+	return 0;
+}
---
+++ ex5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	int b;
+
+	printf("Enter length: ");
+	scanf("%d", &a);
+	printf("Enter width: ");
+	scanf("%d", &b);
+	printf("The square is: %d\n", a*b);
+
+	return 0;
+}
