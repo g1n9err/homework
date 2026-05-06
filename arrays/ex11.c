0158ab3461ddd085499773c4a4311606471d0f5a
g1n9err <chy.korn@proton.me>
2026-05-06 23:14:30 +0400

    arrays 11

--- arrays/ex11.c
+++ arrays/ex11.c
@@ -4,15 +4,17 @@ int main()
 {
     int arr[5];
     int zaza;
+    int count = 0;
     for(int x = 0; x < 5; x++) {
         scanf("%d", &arr[x]);
     }
     for(int x = 0; x < 5; x++) {
         if(arr[x] % 2 != 0) {
             zaza = arr[x];
-             printf("%d\t",zaza);
+            count ++;
         }
     }
+    printf("Odd number count: %d\n", count);
     return 0;
 }
 
