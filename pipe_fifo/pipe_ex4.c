0998c070e9e18682fa7048fee036f132f7c0e799
g1n9err <chy.korn@proton.me>
2026-07-16 20:48:13 +0400

    add pipe exercises 3,file init

--- pipe_fifo/pipe_ex4.c
+++ pipe_fifo/pipe_ex4.c
@@ -1,2 +1,86 @@
+
 #include <stdlib.h>
-#includ
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <string.h>
+
+#define SIZE 1024
+
+typedef struct {
+    int letters;
+    int digits;
+    int spaces;
+    int lines;
+} TextStats;
+
+int main () {
+    int p2c[2];
+    int c2p[2];
+
+    if(pipe(p2c) == -1) {
+        perror("pipe");
+        return 1;
+    }
+    
+    if(pipe(c2p) == -1) {
+        perror("pipe");
+        return 1;
+    }
+    
+    pid_t pid;
+    pid = fork();
+    if(pid < 0){
+        perror("fork");
+        return 1;
+    }
+    TextStats stats = {0,0,0,0};
+
+    if(pid > 0){
+        close(p2c[0]);
+        close(c2p[1]);
+        const char *pr_buf = "CHLP mlp 2.4 cpp\nchlpmlp\n12 34";
+        write(p2c[1],pr_buf,strlen(pr_buf) + 1);
+        close(p2c[1]);
+        wait(NULL);
+        read(c2p[0], &stats, sizeof(stats));
+        printf("letters = %d\n",stats.letters);
+        printf("digits = %d\n", stats.digits);
+        printf("spaces = %d\n", stats.spaces);
+        printf("lines = %d\n",stats.lines);
+
+    }else if(pid == 0) { 
+        close(p2c[1]);
+        close(c2p[0]); 
+        char ch_buffer[SIZE];
+        ssize_t bytes_read;
+        while((bytes_read = read(p2c[0],ch_buffer,sizeof(ch_buffer))) != 0) {
+            if(bytes_read == -1) {
+                if(errno == EINTR) {
+                    continue;
+                }
+                perror("read");
+                break;
+            } 
+
+        for(int i = 0; i <  bytes_read; ++i){
+            char simb = ch_buffer[i];
+            if(simb >= '0' && simb <= '9'){
+                stats.digits ++;
+            } else if(simb == ' ') {
+                stats.spaces ++;
+            } else if(simb >= 'a' && simb <= 'z' ||  simb >= 'A' && simb <= 'Z'){
+                stats.letters ++;
+            } else if(simb == '\n'){
+                stats.lines ++;
+            }
+        }
+    }
+        close(p2c[0]);
+        write(c2p[1], &stats,sizeof(stats));
+        close(c2p[1]);
+    }
+    return 0;
+}
---
+++ pipe_fifo/pipe_ex5.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+#define SIZE 1024
+
+typedef struct 
+{
+    float celsius;
+}Temperature;
+
+typedef struct 
+{
+    float fahrenheit;
+}ConvertedTemperature;
+
+int main() {
+    int p2c[2];
+    int c2p[2];
+    
+    if(pipe(p2c) == -1){
+        perror("pipe");
+        return 1;
+    }
+    if(pipe(c2p) == -1){
+        perror("pipe");
+        return 1;
+    }
+    
+    Temperature temp;
+    printf("Write the temperature in Celsius։ ");
+    scanf("%f", &temp.celsius);
+    printf("\n");
+    ConvertedTemperature conv_temp;
+    
+    pid_t pid = fork();
+    if(pid == -1){
+        perror("fork");
+        return 1;
+    }
+
+    if(pid > 0) {
+        close(p2c[0]);
+        close(c2p[1]);
+        
+        write(p2c[1],&temp,sizeof(Temperature));
+        
+        //puts("greci");
+        
+        close(p2c[1]);
+        //puts("z");
+        wait(NULL);
+        //puts("zz");
+        read(c2p[0],&conv_temp,sizeof(ConvertedTemperature));
+        //puts("zzz");
+        close(c2p[0]);
+        
+        //puts("kardaci");
+        
+        printf("Temperature in Fahrenheit: %.2f\n", conv_temp.fahrenheit);
+    } else if (pid == 0) {
+        printf("mtela ste\n");
+        close(c2p[0]);
+        close(p2c[1]);
+        //puts("g");    
+        read(p2c[0],&temp,sizeof(Temperature));
+        //puts("erexen kardac");
+        
+        
+        conv_temp.fahrenheit = (temp.celsius * 1.8) + 32;
+        close(p2c[0]);
+        
+        write(c2p[1],&conv_temp,sizeof(ConvertedTemperature));
+        //puts("gg");
+        close(c2p[1]);
+        
+        puts("erexen grec");
+    }
+    
+    return 0;
+}
